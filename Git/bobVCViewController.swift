f31ce4e59f1cbf7b0308f8cce2e6fe34de732c37
Timothy P. Hennig <paul.hennig@p-mba-hennig.d214.org>
2018-07-12 13:18:48 -0500

    BobVC

---
+++ Git/bobVCViewController.swift
@@ -0,0 +1,21 @@
+//
+//  bobVCViewController.swift
+//  Git
+//
+//  Created by Timothy P. Hennig on 7/12/18.
+//  Copyright © 2018 Hennig. All rights reserved.
+//
+
+import UIKit
+
+class bobVCViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+        
+    }
+}
